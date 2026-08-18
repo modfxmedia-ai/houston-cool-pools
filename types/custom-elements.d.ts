43d510c6341bfff79ddef730fbe2008f20337a8b
Nargis Fatima <nargis@modfxmedia.com>
2026-08-19 03:00:08 +0500

    fix: correct syntax in custom element type declaration

--- types/custom-elements.d.ts
+++ types/custom-elements.d.ts
@@ -3,10 +3,7 @@ import * as React from "react";
 declare module "react" {
   namespace JSX {
     interface IntrinsicElements {
-      "emr-simple-carousel": React.DetailedHTMLProps
-        React.HTMLAttributes<HTMLElement>,
-        HTMLElement
-      > & {
+      "emr-simple-carousel": React.HTMLAttributes<HTMLElement> & {
         "widget-id"?: string;
       };
     }
